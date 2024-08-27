c818134ef25d9b964f36582b1d744df5b7c94a06
khahussein <kherld.hussein@gmail.com>
2024-08-27 09:31:46 +0300

    Feat(day1): Implement Not Quite Lisp; Floor count;

--- day_1/main.go
+++ day_1/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading Input data")
+		return
+	}
+	f := NotQuiteLisp(string(input))
+	fmt.Printf("Floor: %v", f)
+	fmt.Println()
+}
+
+func NotQuiteLisp(s string) int {
+	f := 0
+	for _, x := range s {
+		if x == '(' {
+			f++
+		} else if x == ')' {
+			f--
+		}
+	}
+	return f
 }
