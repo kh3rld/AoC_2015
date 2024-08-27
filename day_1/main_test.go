93846cd22aa29c5f4006853e4bd111e17ad30c02
khahussein <kherld.hussein@gmail.com>
2024-08-27 12:42:48 +0300

    Feat(test): Implement floor count test2

--- day_1/main_test.go
+++ day_1/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNotQuiteLisp(t *testing.T) {
 	tests := []struct {
@@ -15,6 +17,22 @@ func TestNotQuiteLisp(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NotQuiteLisp(tt.name); got != tt.want {
 				t.Errorf("NotQuiteLisp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNotQuiteLisp2(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"()))", 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NotQuiteLisp2(tt.name); got != tt.want {
+				t.Errorf("NotQuiteLisp2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
