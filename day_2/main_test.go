0d7a236ac46116f5023a3ee52aff8e96efb22373
khahussein <kherld.hussein@gmail.com>
2024-08-28 17:52:44 +0300

    Feat(test): Implement Perimeter area test

--- day_2/main_test.go
+++ day_2/main_test.go
@@ -2,9 +2,17 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
+func TestSurfaceArea(t *testing.T) {
 	want := SurfaceArea("2x3x4")
 	got := 58
+	if want != got {
+		t.Errorf("Expected %v, Got %v", want, got)
+	}
+}
+
+func TestSurfaceArea2(t *testing.T) {
+	want := SurfaceArea2("1x1x10")
+	got := 14
 	if want != got {
 		t.Errorf("Expected %v, Got %v", want, got)
 	}
