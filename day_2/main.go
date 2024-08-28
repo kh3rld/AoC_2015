d7c6480acbd1613f4fe5e4d3164d22fb524cd7c3
khahussein <kherld.hussein@gmail.com>
2024-08-28 12:57:41 +0300

    Feat(main): Implement SurfaceArea calculation

--- day_2/main.go
+++ day_2/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	data, _ := os.ReadFile("input.txt")
+	s := strings.Split(string(data), "\n")
+	var result int
+	for _, x := range s {
+		result += SurfaceArea(x)
+	}
+	fmt.Println(result)
+}
+
+func SurfaceArea(s string) int {
+	ss := strings.Split(s, "x")
+	l, _ := strconv.Atoi(ss[0])
+	w, _ := strconv.Atoi(ss[1])
+	h, _ := strconv.Atoi(ss[2])
+	length := 2 * (l * w)
+	width := 2 * (w * h)
+	height := 2 * (h * l)
+	slack := (min(length, min(width, height))) / 2
+	r := length + width + height
+	return r + slack
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
