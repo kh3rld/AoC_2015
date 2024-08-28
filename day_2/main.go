c8a2461dbdf7c9126379d232b4258d0a9a6f8c32
khahussein <kherld.hussein@gmail.com>
2024-08-28 17:52:23 +0300

    Feat(main): Implement Perimeter calculation

--- day_2/main.go
+++ day_2/main.go
@@ -11,10 +11,13 @@ func main() {
 	data, _ := os.ReadFile("input.txt")
 	s := strings.Split(string(data), "\n")
 	var result int
+	var result2 int
 	for _, x := range s {
 		result += SurfaceArea(x)
+		result2 += SurfaceArea2(x)
 	}
 	fmt.Println(result)
+	fmt.Println(result2)
 }
 
 func SurfaceArea(s string) int {
@@ -35,4 +38,16 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func SurfaceArea2(s string) int {
+	ss := strings.Split(s, "x")
+	l, _ := strconv.Atoi(ss[0])
+	w, _ := strconv.Atoi(ss[1])
+	h, _ := strconv.Atoi(ss[2])
+	length := 2 * (l + w)
+	width := 2 * (w + h)
+	height := 2 * (h + l)
+	slack := (min(length, min(width, height)))
+	return slack + l*w*h
 }
