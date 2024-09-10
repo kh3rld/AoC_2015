bc753318e04a0c31113597678648a8667d2a21d8
khahussein <kherld.hussein@gmail.com>
2024-09-11 00:29:42 +0300

    Refactor(main): Enhance file handling and code clarity. - Added error handling for file operations to manage potential issues with file access. - Removed redundant check in the loop that was unnecessary. - Moved the  function call outside of the loop for efficiency. - Renamed variables for better readability and clarity. - Updated function names and comments to improve code readability.

--- day_5/main.go
+++ day_5/main.go
@@ -1,42 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	data, _ := os.ReadFile("input.txt")
-	s := strings.Split(string(data), "\n")
-	c := 0
-	for _, x := range s {
-		if SortStr(x) {
-			c++
-		}
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
 	}
-	fmt.Println(c)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var data []string
+
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	validCount := SortStr(data)
+	fmt.Println(validCount)
 }
 
-func SortStr(s string) bool {
-	vow := "aeiouAEIOU"
-	ban := []string{"ab", "cd", "pq", "xy"}
+func SortStr(str []string) int {
 	count := 0
+	for _, s := range str {
+		if Vow3ls(s) && Double(s) && Ban(s) {
+			count++
+		}
+	}
+	return count
+}
+
+func Vow3ls(s string) bool {
+	vowels := "aeiou"
+	count := 0
+	for _, v := range vowels {
+		if strings.ContainsRune(s, v) {
+			count++
+		}
+	}
+	return count >= 3
+}
+
+func Double(s string) bool {
 	for i := range s {
-		for _, v := range vow {
-			for _, b := range ban {
-				if strings.ContainsRune(s, v) {
-					count++
-				}
-				if count >= 3 {
-					if i > 0 && s[i] == s[i-1] {
-						if strings.Contains(s, b) {
-							return true
-						}
-					}
-				}
-			}
+		if i > 0 && s[i] == s[i-1] {
+			return true
 		}
 	}
 	return false
+}
+
+func Ban(s string) bool {
+	banned := []string{"ab", "cd", "pq", "xy"}
+	for _, b := range banned {
+		if strings.Contains(s, b) {
+			return false
+		}
+	}
+	return true
 }
